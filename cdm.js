f608cc7fa0d768dd184681983b5e54105aaf77ad
Andres Bastias <106349694+andresMRoblesistemas@users.noreply.github.com>
2022-08-12 16:41:05 -0300

    cambio de nombre de classes y ruta predefinida en el Iframe

--- cdm.js
+++ cdm.js
@@ -3,16 +3,16 @@ function resizeIframe(obj) {
   }
 
   const params = new URLSearchParams(document.location.search);
-  //params === null ? pagina = "../vistas/bienvenida.html" : pagina = params.get("s");
-  const pagina = params.get("s");
-  const titulo = params.get("t");
+  const pagina = (params.get("s") === null) ? "/vistas/bienvenida.html" : params.get("s");
+  const titulo = (params.get("t") === null) ? "00" : params.get("t");
   contador = 0;
 
   function cargarRutaEnMarco(pagina){
       if (contador<1) {
           document.getElementById("vista").src = pagina+'#'+titulo;
+          console.log(document.getElementById("vista").src);
           contador++;
       } else {
-          pagina = "../vistas/bienvenida.html";
+          pagina = "404.html"
       }
   }
